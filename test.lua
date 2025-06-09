5ecacf7009095fc6efe0af9ee5df85b8c792013e
XRZV <157772865+noggik@users.noreply.github.com>
2025-06-09 19:35:01 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,27 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Xin" .. Fluent.Version,
-    SubTitle = "by xin",
+    Title = "Grow a Garden" .. Fluent.Version,
+    SubTitle = "by Xin",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
-    Theme = "Aqua", -- "Dark", "Light", "Darker", "Rose", "Aqua", "Amethyst"
+    Theme = "Aqua",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
-local Options = Fluent.Options
+local Tabs = {
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
